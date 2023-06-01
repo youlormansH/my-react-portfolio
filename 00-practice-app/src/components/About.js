b1c9e9316c54fbb3a0610d403c9cbfd9a7d0d969
youlormansH <128164751+youlormansH@users.noreply.github.com>
2023-05-31 23:16:15 -0400

    working header

--- 00-practice-app/src/components/About.js
+++ 00-practice-app/src/components/About.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id="about-me" className='about intro'>
+      <div class= "flew-row">
+        <h1 id= "about" className="title primary-border">
+          About Me
+        </h1>
+      </div>
+      <div className="flew-row"></div>
+    </section>
+    
   )
 }
 
