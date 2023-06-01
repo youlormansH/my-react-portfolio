aa1642bd61d8b4c2f7c2c34d79029a28f63a6bce
youlormansH <128164751+youlormansH@users.noreply.github.com>
2023-05-31 23:00:29 -0400

    working header

--- 00-practice-app/src/components/Footer.js
+++ 00-practice-app/src/components/Footer.js
@@ -2,14 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="footer" id= "footer">
-      <ul>
-        <li><a href=''
-
-        </li>
-      </ul>
-      
-      Footer</div>
+    <footer className="footer bg-dark fixed-bottom">
+    <a href="https://github.com/youlormansH "><img src="" alt="Github" className="icon"/></a>
+    <a href="linkedin.com/in/youlormanshilaire "><img src="" alt="LinkedIn" className="icon"/></a>
+    <span> © 2023 Youlormans</span> <br />
+</footer>
   )
 }
 
